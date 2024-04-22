93e2297763b3e7e1e6ac8a3cc8efec63e63c1d13
haoxuan <haoxuan1.23@intl.zju.edu.cn>
2024-04-22 16:37:32 +0800

    Initialise

--- Practical 6/City size.py
+++ Practical 6/City size.py
@@ -3,11 +3,19 @@ China_cities=[0.58,8.4,29.9,22.2]
 print(uk_cities)
 print(China_cities)
 
+uk_cities_sorted = sorted(uk_cities)
+China_cities_sorted = sorted(China_cities)
+
+print(uk_cities_sorted)
+print(China_cities_sorted)
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 bar_colors = ['tab:red','tab:orange','tab:blue','tab:green']
 fig,(ax,ax1)=plt.subplots(1,2,figsize=(10,5))
+
 UK_city_names =["Edinburgh",'Glasgow', 'Stirling', 'London']
 ax.bar(UK_city_names,uk_cities,label= uk_cities, color=bar_colors )
 ax.set_ylabel('population')
