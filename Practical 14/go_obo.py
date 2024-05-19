3fd5b22da0d8fe1623d80e0cc2e911ffc441533a
haoxuan <haoxuan1.23@intl.zju.edu.cn>
2024-05-19 16:27:51 +0800

    initialise

--- Practical 14/go_obo.py
+++ Practical 14/go_obo.py
@@ -1,81 +1,71 @@
-import xml.dom.minidom as minidom
+import xml.dom.minidom
 import xml.sax
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-# DOM Parsing Function
-def parse_with_dom(file_path):
-    start_time = datetime.now()
-    
-    doc = minidom.parse(file_path)
-    terms = doc.getElementsByTagName('term')
-    
-    counts = {"molecular_function": 0, "biological_process": 0, "cellular_component": 0}
+FILE_NAME = 'C:/Users/17732/Desktop/IBI/IBI1_2023-24/IBI1_2023-24/Practical 14/go_obo.xml'
+ONTOLOGIES = ['molecular_function', 'biological_process', 'cellular_component']
+
+def parse_dom(file_name):
+    dom_tree = xml.dom.minidom.parse(file_name)
+    terms = dom_tree.getElementsByTagName("term")
+    counts = {ontology: 0 for ontology in ONTOLOGIES}
     
     for term in terms:
-        namespace = term.getElementsByTagName('namespace')[0].childNodes[0].data
+        namespace = term.getElementsByTagName("namespace")[0].childNodes[0].data
         if namespace in counts:
             counts[namespace] += 1
-    
-    end_time = datetime.now()
-    duration = (end_time - start_time).total_seconds()
-    return counts, duration
+    return counts
 
-# SAX Handler Class
 class GOHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.current_data = ""
         self.namespace = ""
-        self.counts = {"molecular_function": 0, "biological_process": 0, "cellular_component": 0}
+        self.counts = {ontology: 0 for ontology in ONTOLOGIES}
     
     def startElement(self, tag, attributes):
         self.current_data = tag
     
-    def endElement(self, tag):
-        if self.current_data == "namespace":
-            if self.namespace in self.counts:
-                self.counts[self.namespace] += 1
-        self.current_data = ""
-    
     def characters(self, content):
         if self.current_data == "namespace":
             self.namespace = content
-
-# SAX Parsing Function
-def parse_with_sax(file_path):
-    start_time = datetime.now()
     
-    handler = GOHandler()
+    def endElement(self, tag):
+        if tag == "namespace":
+            if self.namespace in self.counts:
+                self.counts[self.namespace] += 1
+        self.current_data = ""
+
+def parse_sax(file_name):
     parser = xml.sax.make_parser()
-    parser.setContentHandler(handler)
-    parser.parse(file_path)
-    
-    end_time = datetime.now()
-    duration = (end_time - start_time).total_seconds()
-    return handler.counts, duration
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+    Handler = GOHandler()
+    parser.setContentHandler(Handler)
+    parser.parse(file_name)
+    return Handler.counts
 
-# Plotting Function
-def plot_counts(counts):
-    categories = list(counts.keys())
-    values = list(counts.values())
-    
-    plt.bar(categories, values)
-    plt.xlabel('Ontology')
-    plt.ylabel('Number of GO Terms')
-    plt.title('GO Terms in Each Ontology')
+def plot_data(counts, title):
+    ontologies = list(counts.keys())
+    frequencies = list(counts.values())
+    plt.figure(figsize=(10, 5))
+    plt.bar(ontologies, frequencies, color='skyblue')
+    plt.xlabel('Ontologies')
+    plt.ylabel('Frequency')
+    plt.title(title)
     plt.show()
 
-# Main Execution
-if __name__ == "__main__":
-    file_path = 'go_obo.xml'
-    
-    dom_counts, dom_duration = parse_with_dom(file_path)
-    sax_counts, sax_duration = parse_with_sax(file_path)
-    
-    print(f"DOM Parsing Duration: {dom_duration} seconds")
-    print(f"SAX Parsing Duration: {sax_duration} seconds")
-    
-    # Assuming both should be the same
-    assert dom_counts == sax_counts
-    
-    plot_counts(dom_counts)  
+start_time_dom = datetime.now()
+dom_counts = parse_dom(FILE_NAME)
+time_dom = datetime.now() - start_time_dom
+
+start_time_sax = datetime.now()
+sax_counts = parse_sax(FILE_NAME)
+time_sax = datetime.now() - start_time_sax
+
+print("DOM Counts:", dom_counts)
+print("DOM Time:", time_dom)
+print("SAX Counts:", sax_counts)
+print("SAX Time:", time_sax)
+
+plot_data(dom_counts, 'DOM Parsing Results')
+plot_data(sax_counts, 'SAX Parsing Results')
